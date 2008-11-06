3153608eb1a97a09f78d2ee10857f642621def0a
Trek Glowacki <pietrekg@umich.edu>
2008-11-06 11:30:14 -0500

    Added documentation for CSS value transformers

--- transform.rb
+++ transform.rb
@@ -90,9 +90,7 @@ class Transform
 			return -(Math.cos(Math.PI * p) - 1) / 2;
 		}`
     self.start_timer
-		
 		self.fire(:start)
-		
 		return self
 	end
 		
@@ -132,7 +130,14 @@ class Transform
 		return true
 	end
   
+
+  # Module +Parser+ is responsible for turning a value into a computable value (integer or array of integers),
+  # computing this value to the correct 
   module Parser
+    
+    # Class Color is responsible for handling css color values in either hex (#fff) or rgb triplet (rgb(0,0,0))
+    # formats by parsing them to an array of numbers, transforming each elemement in the array during a transition
+    # and finally serving the then number at the end of a series of transforms.
     class Color
       def self.hex_to_array(color)
         `var hex = color.match(/^#?(\\w{1,2})(\\w{1,2})(\\w{1,2})$/).slice(1)`
@@ -145,6 +150,17 @@ class Transform
     		`rgb`
       end
       
+      # call-seq:
+      #   Parser::Color.compute(array,array,float) -> string
+      # 
+      # Computes from an array of current values to an array of new values
+      # based on a delta by computing each value in the array.
+      # returns a css-compatible rgb triplet string (rgb(00,00,00))
+      # 
+      # In a sequence of steps converting from rgb(0,0,0) to rgb(240,240,240)
+      # Parser::Color.compute may be called with
+      # Parser::Color.compute([0,0,0], [240,240,240], 0.8272711231312) and will
+      # return 'rbg(199,199,199)'
       def self.compute(from, to, delta)
   			rgb = []
   			from.each do |i|
@@ -153,38 +169,77 @@ class Transform
         `'rgb(' + rgb + ')'`
       end
       
-      def self.parse(value)
+      # call-seq:
+      #   Parser::Color.parse(str) -> array or false
+      # Parses a css color value from either hex (#f0f0f0) or rgb triplet (rbg(240,240,240)) to an array
+      # of rgb values or returns false if the value cannot be parsed 
+      #
+      # Parser::Color.parse('#fff')   # => [255,255,255]
+      # Parser::Color.parse('f0f0f0') # => [240,240,240]
+      # Parser::Color.parse('rbg(240,240,240)) # => [240,240,240]
+      #  
+      # Parser::Color.parse('40px')   # => false
+      # Parser::Color.parse('left')   # => false
+      def self.parse(value) # :nodoc
         `value = value.__value__ || String(value)`
         `if (value.match(/^#[0-9a-f]{3,6}$/i)) return #{Transform::Parser::Color.hex_to_array(value)}`
   			`((value = value.match(/(\\d+),\\s*(\\d+),\\s*(\\d+)/))) ? #{[value[1], value[2], value[3]]} : false`
       end
       
+      # Returns the final value in rgb triplet format at the termination of a series of transformations
+      # The second argument (unit) exists for polymorphic calls and is not used.
       def self.serve(value, unit)
         value
       end
     end
     
+    # Class Color is responsible for handling css numbers values with our without units
+    # parsing them, computing new values, and returning a final value with its intial unit reapplied.
     class Number
+      # call-seq:
+      #   Parser::Number.compute(number,number,float) -> string
+      # 
+      # Computes from an current value to a newvalue 
+      # based on a delta and returns a css compatible string.
+      # 
       def self.compute(from,to,delta)
         Transform.compute(from,to,delta)
       end
       
+      # call-seq:
+      #   Parser::Number.parse(str or number) -> number or false
+      # Parses a number value from as either a string or number and returns an integer
+      # that will later be transformed.
+      # Returns false if the value cannot be parsed as a number
+      #
+      # Parser::Number.parse(20)   # => 20
+      # Parser::Number.parse(0)    # => 0
+      # Parser::Number.parse('220px')    # => 220
+      #  
+      # Parser::Number.parse('left')   # => false
       def self.parse(value)
         `value = value.__value__ || String(value)`
         `parsed = parseFloat(value)`
         `(parsed || parsed == 0) ? parsed : false`
       end
       
+      # Returns the final value as a string at the termination of a series of transformations
+      # with its inital unit reapplied
+      # 
+      # Parser::Number.serve(20,'px') # => '20px'
+      # Parser::Number.serve(40, nil) # => '40
+      # The second argument (unit) exists for polymorphic calls and is not used.
       def self.serve(value, unit)
         return (unit) ? value + unit : value
       end
     end
     
-    # can't tween a string
-    class String
+    # Class String is responsible for handling css string values which cannot be parsed. Simple transforms
+    # from a start value to end value with no transform.
       def self.parse(value)
         false
       end
+      
       def self.compute(from, to, delta)
         return to        
       end
